c0557dd6c84838ca1103529f6a1fcc7830344c3a
biyrololo <92595932+biyrololo@users.noreply.github.com>
2024-02-24 15:08:46 +0400

    s

--- server/db.py
+++ server/db.py
@@ -2,7 +2,6 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 # DATABASE_URL = "postgresql://postgres:rootroot@localhost:5433/gen-images-site"
-DATABASE_URL = 'postgresql://admin:wQh6FuPDLLCl0B1aSKd0ukoEQEmeHTuw@dpg-cn8b2o21hbls73d9onp0-a.oregon-postgres.render.com/dbmain_hloj'
 from sqlalchemy import create_engine, text, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
